ddc4de54265bb20119cf033c9af12953bfbaae42
Abdurahim <abdujalilovabdurahim72@gmail.com>
2025-04-15 10:47:20 +0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\TasksController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+});
 
 Route::apiResource('tasks', TasksController::class);
